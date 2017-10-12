2e108fc92f87857b03e829dd3f9101fef091b74b
Morgan Greff <morgan@greffs.com>
2017-10-12 17:10:05 +0000

    Emails order details to user

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,6 +4,7 @@ class OrderMailer < ApplicationMailer
  def order_email(order, user)
    @user = user
    @subject = "Order: #{order.id}"
+   @order = order
    mail(to: @user.email, subject: @subject)
  end
 end
