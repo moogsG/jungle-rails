55af550d10a20ef6259932debc9e43c2c90c0b1e
Morgan Greff <morgan@greffs.com>
2017-10-16 18:53:07 +0000

    Added indvidual tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,8 +6,33 @@ RSpec.describe Product, type: :model do
         product = Product.new(name: "Bob", price: 300, quantity: 4, category_id: 0, category: Category.new)
         expect(product).to be_valid
       end
-    describe 'name' do
-
+    describe 'Valid name' do
+      it "name is not valid" do
+        product = Product.new(price: 300, quantity: 4, category_id: 0, category: Category.new)
+        expect(product).to_not be_valid
+        expect(product.errors.messages[:name]).to include('can\'t be blank')
+      end
+    end
+    describe 'Valid price' do
+      it "price is not valid" do
+        product = Product.new(name: "Bob", quantity: 4, category_id: 0, category: Category.new)
+        expect(product).to_not be_valid
+        expect(product.errors.messages[:price]).to include('can\'t be blank')
+      end
+    end
+    describe 'Valid quantity' do
+      it "quantity is not valid" do
+        product = Product.new(price: 300, name: "Bob", category_id: 0, category: Category.new)
+        expect(product).to_not be_valid
+        expect(product.errors.messages[:quantity]).to include('can\'t be blank')
+      end
+    end
+    describe 'Valid category' do
+      it "category is not valid" do
+        product = Product.new(price: 300, quantity: 4, category_id: 0, name: "Bob")
+        expect(product).to_not be_valid
+        expect(product.errors.messages[:category]).to include('can\'t be blank')
+      end
     end
   end
 end
